9cfa934dca179a66ac302bd8a570d4f3f79913a2
Adrian Edwards <adrian@adriancedwards.com>
2024-10-26 12:06:06 -0400

    parameterize cellref

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Google Sheets API URL (replace with your sheet's ID and range)
 // https://docs.google.com/spreadsheets/d/e/2PACX-1vSAomwWPDZwgj-AIooKULhoeoAGfIJH4ufo2cPX8R0_i23Kf6xwyTUYUftFM8UVQw1DTZFP0_fDzqj9/pubhtml
-var googleSheetURL = "https://sheets.googleapis.com/v4/spreadsheets/1UfYzv9pCvi-8h8b3yWIFH1DT1rXiPF7oIChlKXW-mvU/values/'Direct Giving Links'!A2:A500?key=AIzaSyAJOC5ss6cLfe9i-X4J2u-qzE_ZP8n3Ubk";
+
 
 // https://docs.google.com/spreadsheets/d/e/2PACX-1vSAomwWPDZwgj-AIooKULhoeoAGfIJH4ufo2cPX8R0_i23Kf6xwyTUYUftFM8UVQw1DTZFP0_fDzqj9/pubhtml
 
-googleSheetURL = encodeURI(googleSheetURL)
 
 // Fetch data from Google Sheets
-async function fetchData() {
+async function fetchData(cellref = "'Direct Giving Links'!A2:A500") {
+	var googleSheetURL = `https://sheets.googleapis.com/v4/spreadsheets/1UfYzv9pCvi-8h8b3yWIFH1DT1rXiPF7oIChlKXW-mvU/values/${cellref}?key=AIzaSyAJOC5ss6cLfe9i-X4J2u-qzE_ZP8n3Ubk`;
 	try {
 		const response = await fetch(googleSheetURL);
 		const data = await response.json();
